503011a702c116bc435928ba23d51674e153e19f
AIshutin <hazmozavr@gmail.com>
2018-11-10 17:35:19 +0300

    Update ruwiktionary.py

--- ruwiktionary.py
+++ ruwiktionary.py
@@ -5,18 +5,216 @@
 COMP
 женись
 """
+
 import requests as rq
 import bs4
 import json
 import random as rd
-from time import sleep
-from fake_useragent import UserAgent
 import os
+from copy import deepcopy
 
+from time import sleep
+import pprint
+import pymorphy2 as py
 
 stress_char = "ре́с"[2]
 not_found = "@"
-vowels = "уяаюыеи"
+morph = py.MorphAnalyzer()
+
+def normal_form(stressed):
+	return stressed.replace(stress_char, '')
+
+def is_stressed(form):
+	return form.find(stress_char) != -1
+
+def make_stressed(word):
+    """
+    A function that marks first vowel as stressed
+    :param word
+    :return stressed word
+    """
+    ans = ""
+    was = False
+    for el in word:
+        ans += el
+        if el in vowels and not was:
+            ans += stress_char
+            was = True
+    return ans
+
+def get_text(bs):
+	text = []
+	state = 0
+	'''
+	0 1 1 0
+	ab<cd >
+	'''
+	curr = str(bs).replace('<br/>', ' ')
+	for el in curr:
+		if state == 0:
+			if el == '<':
+				state = 1
+			else:
+				text.append(el)
+		elif state == 1:
+			if el == '>':
+				state = 0
+	return "".join(text)
+
+def clean_word(stressed):
+	res = []
+	for el in stressed:
+		if el.isalpha() or el == stress_char:
+			res.append(el)
+	return ''.join(res)
+
+def find_formstress(table):
+    """
+    A function to find the stress of the some word form.
+    :param word - Parse object
+    :param table - parsed html tables array with stress information
+    :return - word with stress
+    """
+    if table is None:
+        return dict()
+
+    final = set()
+
+    for POS in {'NOUN', 'ADJF', 'ADJS', 'PRTF', 'PRTS', 'VERB', 'GRND', 'ADVB'}:
+	    if POS == 'NOUN':
+	        need = table[POS]
+	        whois = 'N'
+	    elif POS in {'ADJF', 'ADJS', 'PRTF', 'PRTS'}:
+	        need = table['ADJ']
+	        whois = 'A'
+	    elif POS in {'VERB', 'GRND'}:
+	        need = table['VERB']
+	        whois = 'V'
+	    else:
+	        need = table['ADVB']
+	        whois = 'O' #others
+	    if len(need) > 1:
+	        need = need[:1]
+	    if len(need) == 0:
+	    	continue
+
+	    need = need[0]
+
+	    eng2rus = {'nomn': 'именительный', 'gent': 'родительный', 
+	               "datv": "дательный", "accs": "винительный",
+	               "ablt": "творительный", "loct": "предложный",
+	               "gen2": "родительный", "loc2": "предложный",
+	               "voct": "звательный падеж", None: "краткая форма"}
+	    genders = {'masc': 0, 'femn': 2, 'neut': 1, None: 3}
+
+	    if whois == 'N':
+	        dictlike = dict()
+	        for tr in need.find_all('tr'):
+	            td = tr.find_all('td')
+	            if len(td) == 0:
+	                continue
+	            formes = [get_text(el).rstrip() for el in td[1:3]]
+	            for form in formes:
+	                final.update(form.split())
+	    elif whois == 'A':
+	        for tr in need.find_all('tr'):
+	            td = tr.find_all('td')
+	            if len(td) == 0:
+	                continue
+	            formes = [get_text(el).rstrip() for el in td[1:5]]
+	            for form in formes:
+	            	final.update(form.split())
+	    elif whois == 'V':
+	        present = {('2per', 'sing'): "ты", ('1per', 'sing'): "я", 
+	                        ('3per', 'sing'): "он", ('1per', 'plur'): "мы", 
+	                        ('2per', 'plur'): "вы", ('3per', 'plur'): "они"}
+	        for tr in need.find_all('tr'):
+	            td = tr.find_all('td')
+	            if len(td) == 0:
+	                continue
+	            title = (td[0].find('a')).attrs['title']
+	            formes = [get_text(el).rstrip() for el in td[1:4]]
+	            for form in formes:
+	            	final.update(form.split())
+	    else:
+	        final.add(need)
+    clear = dict()
+    for el in final:
+    	if is_stressed(el):
+    		el = clean_word(el)
+    		clear[normal_form(el)] = el
+    return clear
+
+class StressDB:
+    articles = dict()
+
+    def load(self, filename):
+        with open(filename, encoding='utf-8') as file:
+            self.articles = json.loads(file.read())
+
+    def __init__(self, source=None):
+        self.articles = dict()
+        if source is not None:
+            self.load(source)
+
+    def save(self, filename):
+        with open(filename, 'w') as file:
+            print(json.dumps(self.articles), file=file)
+
+    def get_stress(self, parse):
+	    objhash = parse.normal_form
+	    if objhash not in self.articles:
+	       	try:
+	       		res = find_formstress(get_stress(parse))
+	       	except Exception as exp:
+	       		if exp.args[0] != 'Page not found':
+	       			raise
+	       		print(exp)
+	       		return not_found
+	        for form in res:
+	        	self.articles[form] = res[form]
+	    if parse.word in self.articles:
+	    	return self.articles[parse.word]
+	    else:
+	   		return not_found
+
+    def __len__(self):
+        return len(self.articles)
+
+def get_stress(word):
+    """
+    Function to get stress.
+    :param word: Parse object from pymorphy2
+    :return: index of stressed letter
+    """
+
+    if "LATN" in word.tag:
+        raise Exception("Page not found")
+
+    url = "https://ru.wiktionary.org/wiki/"
+    normal = word.normal_form
+
+    endings = {'а', 'ы', "о"}
+
+    if word.tag.POS == 'PRTF':
+        normal = word.word[:-2] + 'ый'
+    elif word.tag.POS == 'PRTS':
+        normal = word.word
+        if normal[-1] in endings:
+            normal = normal[:-1]
+        normal += 'ный'
+    print(normal, word.tag, flush=True)
+    #try:
+    print("SENT", flush=True)
+    resp = rq.get(url + normal)
+    print("GOT", flush=True)
+    print(resp.status_code, flush=True)
+    #except:
+    #    raise Exception("Timeout")
+    if (resp.status_code != 200):
+        raise Exception("Page not found")
+    bs = bs4.BeautifulSoup(resp.text, 'html.parser')
+    return get_essences(bs, normal)
 
 def get_essences(article, word):
     """
@@ -67,345 +265,7 @@ def get_essences(article, word):
     interesting['ADVB'] = poss
     return interesting
 
-def make_stressed(word):
-    """
-    A function that marks first vowel as stressed
-    :param word
-    :return stressed word
-    """
-    ans = ""
-    was = False
-    for el in word:
-        ans += el
-        if el in vowels and not was:
-            ans += stress_char
-            was = True
-    return ans
-
-def check(word, stressed):
-    """
-    Checks if stressed form is related to word.
-    :param word - a word without stress
-    :param stressed - stressed form of a word
-    :return bool
-    """
-    d = dict()
-    for el in word:
-        if el not in d:
-            d[el] = 0
-        d[el] += 1
-    for el in stressed:
-        if stress_char == el:
-            continue
-        if el not in d:
-            return False
-        d[el] -= 1
-    for el in d:
-        if d[el] != 0:
-            return False
-    return True
-
-
-def find_formstress(word, table):
-    """
-    A function to find the stress of the some word form.
-    :param word - Parse object
-    :param table - parsed html tables array with stress information
-    :return - word with stress
-    """
-    if table is None:
-        return not_found
-    if sum(word.word.count(vow) for vow in vowels) <= 1:
-        return make_stressed(word.word)
-
-    POS = word.tag.POS 
-    need = whois = None
-
-    if POS == 'NOUN':
-        need = table[POS]
-        whois = 'N'
-    elif POS in {'ADJF', 'ADJS', 'PRTF', 'PRTS'}:
-        need = table['ADJ']
-        whois = 'A'
-    elif POS in {'VERB', 'GRND'}:
-        need = table['VERB']
-        whois = 'V'
-    else:
-        need = table['ADVB']
-        whois = 'O' #others
-
-    #print(need)
-    if len(need) > 1:
-        need = need[:1]
-        #raise Exception("Too many variants", need)
-    if len(need) == 0:
-        return not_found
-        raise Exception('No variants')
-
-    need = need[0]
-
-    eng2rus = {'nomn': 'именительный', 'gent': 'родительный', 
-               "datv": "дательный", "accs": "винительный",
-               "ablt": "творительный", "loct": "предложный",
-               "gen2": "родительный", "loc2": "предложный",
-               "voct": "звательный падеж", None: "краткая форма"}
-    genders = {'masc': 0, 'femn': 2, 'neut': 1, None: 3}
-
-    if whois == 'N':
-        dictlike = dict()
-        for tr in need.find_all('tr'):
-            td = tr.find_all('td')
-            if len(td) == 0:
-                continue
-            formes = [el.text.rstrip() for el in td[1:3]]
-            title = (td[0].find('a')).attrs['title']
-            dictlike[title] = formes
-        res = dictlike[eng2rus[word.tag.case]][0 if word.tag.number != 'plur' else 1]
-        if check(word.word, res):
-            return res
-        else:
-            return not_found
-    elif whois == 'A':
-        dictlike = dict()
-        for tr in need.find_all('tr'):
-            td = tr.find_all('td')
-            if len(td) == 0:
-                continue
-            formes = [el.text.rstrip() for el in td[1:5]]
-            title = (td[0].find('a')).attrs['title']
-            dictlike[title] = formes
-        try:
-            res = dictlike[eng2rus[word.tag.case]][genders[word.tag.gender]]
-        except:
-            return not_found
-        if not check(word.word, res):
-            return not_found
-        return res
-    elif whois == 'V':
-        dictlike = dict()
-        present = {('2per', 'sing'): "ты", ('1per', 'sing'): "я", 
-                        ('3per', 'sing'): "он", ('1per', 'plur'): "мы", 
-                        ('2per', 'plur'): "вы", ('3per', 'plur'): "они"}
-        past_gend = [None] * 3
-        for tr in need.find_all('tr'):
-            td = tr.find_all('td')
-            if len(td) == 0:
-                continue
-            title = (td[0].find('a')).attrs['title']
-            if title == 'он':
-                s = str(td[2])
-                s = s[s.find('>') + 1:s.rfind('<')]
-
-                past_gend = [el.rstrip() for el in list(s.split('<br/>'))]
-                    
-            formes = [el.text.rstrip() for el in td[1:4]]
-            dictlike[title] = formes
-
-        genders = {'masc': 0, 'femn': 1, 'neut': 2, None: 3}
-
-        if word.tag.POS == 'GRND':
-            if ', ' in dictlike["деепричастие"][0]:
-                dictlike['деепричастие'] = dictlike['деепричастие'][0].split(', ')
-            for form in dictlike['деепричастие']:
-                if check(word.word, form):
-                    return form
-            return not_found
-        if 'past' in word.tag:
-            if word.tag.gender is None:
-                res = dictlike['они'][1] 
-            else: 
-                res = past_gend[genders[word.tag.gender]]
-            if not check(word.word, res):
-                return not_found
-            return res
-        ind = 0
-        res = ""
-        if 'impr' in word.tag:
-            ind = -1
-            if 'plur' in word.tag:
-                res = dictlike['вы'][-1]
-            else:
-                res = dictlike["ты"][-1]
-        else:
-            res = dictlike[present[(word.tag.person, word.tag.number)]][0]
-        if check(word.word, res):
-            return res
-        else:
-            return not_found
-    else:
-        print(need)
-        if check(word.word, need):
-            return need
-        else:
-            return not_found
-
-
-class ProxyMaster:
-    proxy_list = []
-    last = 0
-    ua = None
-
-    def get_proxy(self):
-        pass
-        self.proxy_list = []
-        url = 'https://www.sslproxies.org/'
-
-        doc = rq.get(url, headers={'User-Agent': self.ua.random})
-
-        soup = bs4.BeautifulSoup(doc.text, 'html.parser')
-        proxies_table = soup.find(id='proxylisttable')
-
-        # Save proxies in the array
-        for row in proxies_table.tbody.find_all('tr'):
-            self.proxy_list.append({
-              'ip':   row.find_all('td')[0].string,
-              'port': row.find_all('td')[1].string
-        })
-
-    def __init__(self):
-        self.last = 0
-        self.ua = UserAgent()
-        #self.get_proxy()
-
-    def feed_with_proxy(self):
-        elem = self.proxy_list[rd.randint(0, len(self.proxy_list) - 1)]
-        proxy = 'https://' + elem['ip'] + ":" + elem['port']
-        return {
-            'http': proxy,
-            'https': proxy
-        }
-
-    def get(self, url):
-        if self.last % 100 == 0:
-            pass
-            #self.get_proxy()
-        
-        self.last += 1
-        #resp = rq.get(url, headers={'User-Agent': self.ua.random})
-        #if resp.status_code != 200:
-        #    print("waited... ")
-        #    sleep(120)
-        #    resp = rq.get(url, )
-        #pr = self.feed_with_proxy()
-        ua = self.ua.random
-        #print(pr, ua)
-        #print(rq.get('http://icanhazip.com', headers={'User-Agent': ua}, proxies=pr).text)
-        return rq.get(url, headers={'User-Agent': ua}, timeout=10)#, proxies=pr)
-
-proxy = ProxyMaster()
-
-def get_stress(word):
-    """
-    Function to get stress.
-    :param word: Parse object from pymorphy2
-    :return: index of stressed letter
-    """
-
-    if "LATN" in word.tag:
-        raise Exception("Page not found")
-
-    url = "https://ru.wiktionary.org/wiki/"
-    normal = word.normal_form
-
-    endings = {'а', 'ы', "о"}
-
-    if word.tag.POS == 'PRTF':
-        normal = word.word[:-2] + 'ый'
-    elif word.tag.POS == 'PRTS':
-        normal = word.word
-        if normal[-1] in endings:
-            normal = normal[:-1]
-        normal += 'ный'
-    print(normal, word.tag, flush=True)
-    #try:
-    print("SENT", flush=True)
-    resp = proxy.get(url + normal)
-    print("GOT", flush=True)
-    print(resp.status_code, flush=True)
-    #except:
-    #    raise Exception("Timeout")
-    if (resp.status_code != 200):
-        raise Exception("Page not found")
-    bs = bs4.BeautifulSoup(resp.text, 'html.parser')
-    return get_essences(bs, normal)
-
-
-class StressDB:
-    articles = dict()
-
-    def load(self, filename):
-        with open(filename, encoding='utf-8') as file:
-            articles = json.loads(file.read())
-            #print(articles)
-            self.articles = dict()
-            for obj in articles:
-                #print("objhashes:", obj)
-                self.articles[obj] = dict()
-                for el in articles[obj]:
-                    #print(el)
-                    if el == 'ADVB':
-                        self.articles[obj][el] = articles[obj][el]
-                        continue
-                    #print(type(articles[obj]), len(articles[obj]))
-                    #print(articles[obj])
-                    #print(articles[obj][el])
-                    self.articles[obj][el] = []
-                    for i in range(len(articles[obj][el])):
-                        self.articles[obj][el].append(bs4.BeautifulSoup(articles[obj][el][i], 'html.parser'))
-            #print(self.articles)
-
-    def __init__(self, source=None):
-        self.articles = dict()
-        if source is not None:
-            self.load(source)
-
-    def save(self, filename):
-        jsonify = dict()
-        for word in self.articles:
-            if self.articles[word] is None:
-                continue
-            jsonify[word] = dict()
-            for tag in self.articles[word]:
-                if tag == 'ADVB':
-                    jsonify[word][tag] = self.articles[word][tag]
-                    continue
-
-                arr = []
-                #print(type(word), type(tag))
-                #print(tag)
-                for el in self.articles[word][tag]:
-                    arr.append(str(el))
-                jsonify[word][tag] = arr
-        #print(jsonify)
-
-        with open(filename, 'w') as file:
-            print(json.dumps(jsonify), file=file)
-        pass
-
-    def get_stress(self, parse):
-        objhash = parse.normal_form
-        table = None
-        if objhash not in self.articles:
-            try:
-                self.articles[objhash] = get_stress(parse)
-            except Exception as exp:
-                if exp.args[0] != 'Page not found' and exp.args[0] != 'Timeout':
-                    raise
-                #print(exp.args[0])
-                self.articles[objhash] = None
-
-        table = self.articles[objhash]
-        print("find_formstress...", flush=True)
-        form = find_formstress(parse, table)
-        print("got it", flush=True)
-        return form
-
-    def __len__(self):
-        return len(self.articles)
-
-
 if __name__ == "__main__":
-    import pymorphy2 as py
     if "stressdb.json" in os.listdir("."):
         db = StressDB("stressdb.json")
         assert(len(db) >= 7)
@@ -430,9 +290,8 @@ if __name__ == "__main__":
             try:
                 print("word:", word, flush=True)
                 print(word, db.get_stress(morph.parse(word)[0]))
-                if (len(db) - last) % 300 == 0 and len(db) != last:
-                    print(len(db))
-                    print("SAVED")
+                if (len(db) - last) % 400 == 0 and len(db) != last:
+                    print("SAVED", len(db))
                     db.save("db-stress-" + str(len(db)) + ".txt")
                     db.save("stressdb.json")
                     db = StressDB('stressdb.json')
@@ -449,12 +308,3 @@ if __name__ == "__main__":
                 last = len(db)
                 sleep(180)
                 print("continued")
-    
-    '''while True:
-        word = input()
-        if word == 'save':
-            db.save(input())
-        elif word == 'load':
-            db.load(input())
-        else:
-            print(db.get_stress(morph.parse(word)[0]))'''
