a51ca04df818b56c9db74d0e9681136dafe9413f
aishutin <hazmozavr@gmail.com>
2018-11-26 09:51:10 +0300

    version number fix

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup is the main function that describes all the package
 setup(
     name='rustress',
     packages=find_packages(exclude=['stressdb.json', 'stressdb.zip', 'Model_creation.h5']),
-    version='1.3.0',
+    version='1.3.2',
     description='A tiny module for offline stress detection in russian words.',
     author='aishutin',
     author_email='hazmozavr@gmail.com',
     url='https://github.com/aishutin/rustress',
-    download_url='https://github.com/aishutin/rustress/archive/1.3.0.tar.gz',
+    download_url='https://github.com/aishutin/rustress/archive/1.3.2.tar.gz',
     keywords=['ru', 'stress', 'rustress', 'poetry', 'linguistic', 'python'],
     install_requires=['requests==2.20.1', 'bs4==0.0.1', 'pymorphy2==0.8', 'tensorflow==1.12.0', 'keras==2.2.4']
 )
